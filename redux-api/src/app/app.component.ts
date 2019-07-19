f1297715ff03e06c8750f92d2d42a9ba3dccd624
Tapion <puntadelanza86@gmail.com>
2019-07-18 21:08:48 -0500

    Segunda seccion del curso sin el nieto

---
+++ redux-api/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  contador: number;
+  constructor() {
+    this.contador = 10;
+  }
+
+  incrementar() {
+    this.contador += 1;
+  }
+
+  decrementar() {
+    this.contador -= 1;
+  }
+}
---
+++ redux-api/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HijoComponent } from './contador/hijo/hijo.component';
+import { NietoComponent } from './contador/nieto/nieto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HijoComponent,
+    NietoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
